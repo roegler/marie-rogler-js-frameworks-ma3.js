c7823ade9db87f2c67a0fb9de5427955f513406f
Marie Røgler <mroegler@gmail.com>
2020-09-28 13:48:01 +0200

    adding props in heading and a list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Heading from "./components/heading/Heading";
+import NewsList from "./components/list/NewsList";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading title="MA3"/>
+
+      <NewsList />
     </div>
   );
 }
---
+++ src/components/list/NewsList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List() {
+    return <ul>
+        <li>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</li>
+        <li>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</li>
+        <li>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</li>
+    </ul>
+}
+
+export default List; 
